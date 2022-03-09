019b081a99199d1e5788e25862d99a6ffae6c8e7
harrisafoster <harrisafoster@gmail.com>
2022-03-09 14:38:56 +0100

    Application 'profiles' créée, modèles/vues déplacées dans la nouvelle application, URLs changés pour utiliser la nouvelle application, migrations customisés utilisés pour migrer les données vers les nouvelles tables, modèles/vues supprimées de l'application initiale, fichiers obsolètes supprimés.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -7,7 +7,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls'), name='lettings'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('profiles/', include('profiles.urls'), name='profiles'),
     path('admin/', admin.site.urls),
 ]
