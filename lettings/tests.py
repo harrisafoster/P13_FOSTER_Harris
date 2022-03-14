3ab6cfc14eb985ff1b1281446b08625d42bcbca8
harrisafoster <harrisafoster@gmail.com>
2022-03-14 10:58:43 +0100

    Suite de tests développée pour les deux applications restantes.

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+from .models import Address, Letting
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.address_object = Address.objects.create(
+            number='4',
+            street='Military Street',
+            city='Willoughby',
+            state='OH',
+            zip_code='44094',
+            country_iso_code='USA',
+        )
+        self.letting_object = Letting.objects.create(title='TestTitle',
+                                                     address=self.address_object)
+        self.url_lettings_index = reverse('lettings_index')
+        self.url_letting = reverse('letting', args=[self.letting_object.id])
+
+    def test_get_lettings_index(self):
+        response = self.client.get(self.url_lettings_index)
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_lettings_index(self):
+        response = self.client.get(self.url_lettings_index)
+        self.assertTemplateUsed(response, 'lettings/index.html')
+
+    def test_title_lettings_index(self):
+        response = self.client.get(self.url_lettings_index)
+        self.assertIn(b'Lettings', response.content)
+
+    def test_get_one_letting(self):
+        response = self.client.get(self.url_letting)
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_one_letting(self):
+        response = self.client.get(self.url_letting)
+        self.assertTemplateUsed(response, 'lettings/letting.html')
+
+    def test_title_one_letting(self):
+        response = self.client.get(self.url_letting)
+        self.assertIn(b'TestTitle', response.content)
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url_profiles_index = reverse('profiles_index')
+
+        self.user_object = User.objects.create(username='testuser', password='PDX1337!')
+        self.profile_object = Profile.objects.create(user=self.user_object,
+                                                     favorite_city='Portland')
+        self.url_profile = reverse('profile', args=[self.user_object.username])
+
+    def test_get_profiles_index(self):
+        response = self.client.get(self.url_profiles_index)
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_profiles_index(self):
+        response = self.client.get(self.url_profiles_index)
+        self.assertTemplateUsed(response, 'profiles/index.html')
+
+    def test_title_profiles_index(self):
+        response = self.client.get(self.url_profiles_index)
+        self.assertIn(b'Profiles', response.content)
+
+    def test_get_one_profile(self):
+        response = self.client.get(self.url_profile)
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_one_profile(self):
+        response = self.client.get(self.url_profile)
+        self.assertTemplateUsed(response, 'profiles/profile.html')
+
+    def test_title_one_profile(self):
+        response = self.client.get(self.url_profile)
+        self.assertIn(b'testuser', response.content)
